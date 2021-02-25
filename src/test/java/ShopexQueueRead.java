a23bf129194543ce66f674e4f84916f1e827f5ed
戴新宇 <daixinyu@shopex.cn>
2021-02-25 10:25:51 +0800

    整理case

---
+++ src/test/java/ShopexQueueRead.java
@@ -0,0 +1,33 @@
+import com.teegon.sdk.util.WebUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import com.teegon.sdk.TeegonClient;
+
+public class ShopexQueueRead {
+
+
+    public static void main(String[] args) {
+
+        String url = "http://apigateway.shopex.cn/router";
+        String appkey = "xx";
+        String secret = "xx";
+        String method = "shopex.queue.read";
+
+        TeegonClient teegonClient = new TeegonClient(url, appkey, secret, Boolean.FALSE);
+        Map<String, String> appParams = new HashMap<String, String>();
+        appParams.put("method", method);
+        appParams.put("topic", "orders");
+        appParams.put("num", "1");
+        try {
+            String apiResult = teegonClient.doPost(appParams);
+            System.out.println(apiResult);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
--- src/test/java/StoreLogisticsOfflineSend.java
+++ src/test/java/StoreLogisticsOfflineSend.java
@@ -1,25 +1,29 @@
-import com.teegon.sdk.util.WebUtils;
+import com.teegon.sdk.TeegonClient;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import com.teegon.sdk.TeegonClient;
 
-public class TeegonPostTest {
+public class StoreLogisticsOfflineSend {
 
 
     public static void main(String[] args) {
 
         String url = "http://apigateway.shopex.cn/router";
-        String appkey = "";
-        String secret = "";
-        String method = "shopex.queue.read";
+        String appkey = "xx";
+        String secret = "xx";
+        String method = "store.logistics.offline.send";
 
         TeegonClient teegonClient = new TeegonClient(url, appkey, secret, Boolean.FALSE);
         Map<String, String> appParams = new HashMap<String, String>();
         appParams.put("method", method);
-        appParams.put("topic", "orders");
-        appParams.put("num", "1");
+        appParams.put("tid", "4767633381843150624A");
+        appParams.put("company_code", "OTHER");
+        appParams.put("company_name", "上海客户自提");
+        appParams.put("logistics_no", "310131872833");
+        appParams.put("node_id", "1665176836_1031190436");
+        appParams.put("request_type", "sync");
+
         try {
             String apiResult = teegonClient.doPost(appParams);
             System.out.println(apiResult);
